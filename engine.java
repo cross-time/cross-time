bac2ad9d7ee859d10e7be933511885be39561534
Kailas2 <kailas@ubuntu.ubuntu-domain>
2013-09-21 15:32:08 +0200

    Creation projet

---
+++ engine.java
@@ -0,0 +1,15 @@
+import fr.EngineLWJGL.Engine.Graphic.MainEngine;
+import fr.EngineLWJGL.Engine.Graphic.Window;
+
+public class engine 
+{
+
+	public static void main(String[] args) 
+	{
+		Window win = new Window();
+		win.createWindow(800, 600, 32, "Engine LWJGL", true);
+		
+		MainEngine engine = new MainEngine();
+		engine.start();
+	}
+}
